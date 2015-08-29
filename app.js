50f4e7f3215fad0cc43830b3411946465fa40150
orep <orep@Il.ibm.com>
2015-08-29 20:38:11 +0300

    Moves the validation to the filter class

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var db = require('./lib/db').connect('weplay_test' , 35693);
+var db = require('./lib/framework/db').connect('weplay_test' , 35693);
 var usersExternal = require('./lib/external/main/usersEndpoint.js');
 var leaguesExternal = require('./lib/external/main/leaguesEndpoint.js');
 
@@ -33,35 +33,10 @@ router.use(authenticate);
 
 
 //middleware to use for all requests
-router.use(function(req, res, next) {
-	logger.audit('use' , req.method + '. New Request from: ' + req.hostname + ' routing to: ' + req.path);
-	validateRequest(req, function( err ) {
-		if(err == null) {
-			logger.audit('use' ,  'Request has passed validation, routing to: ' + req.path);
-			next();
-		} else {
-			logger.error('use' ,  'Request failed validation');
-			res.send(wpresponse.sendResponse(null, err, err.errmsg, false));
-		}
-	});
-});
+router.use(filter.validateRequest);
 
 router.use(leaguesExternal);
 router.use(usersExternal);
-
-//Validate the request
-function validateRequest( req, callback ) {
-	var clientId = req.get(Constants.CLIENT_ID_HEADER_KEY);
-	users.getUserById(clientId, function(err, result) {
-		if( err != null || result == null ) {
-			// No user was found
-			callback( "No Client was found in the DB, error: " + err );
-		} else {
-			// User was found
-			callback(null);
-		}
-	});
-}
 
 app.listen(port);
 logger.audit("listen","Making a smarter planet on port " + port);
