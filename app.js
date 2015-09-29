fc2af3ddd18e211ba30af206d0dd3179edd64ad6
orep <orep@Il.ibm.com>
2015-09-29 20:35:31 +0300

    CORS

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ logger = logger.init('server');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Allow for cross-origin CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(path.ROOT, router); // All requests have wePlay attached
