17c981d1e9f2e6c203c4676846439e3e307d6a0c
yahavw <yahavwin@gmail.com>
2015-11-19 01:21:45 +0200

    init client

---
+++ client/wePlayIonic/www/js/routes.js
@@ -0,0 +1,75 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'loginCtrl'
+    })
+
+
+
+
+
+    .state('tabsController.myProfile', {
+      url: '/profile',
+      views: {
+        'tab1': {
+          templateUrl: 'templates/myProfile.html',
+          controller: 'myProfileCtrl'
+        }
+      }
+    })
+
+
+
+
+
+    .state('tabsController.game', {
+      url: '/game',
+      views: {
+        'tab2': {
+          templateUrl: 'templates/game.html',
+          controller: 'gameCtrl'
+        }
+      }
+    })
+
+
+
+
+
+    .state('tabsController.league', {
+      url: '/league',
+      views: {
+        'tab3': {
+          templateUrl: 'templates/league.html',
+          controller: 'leagueCtrl'
+        }
+      }
+    })
+
+
+
+
+    .state('tabsController', {
+      url: '/page3',
+      abstract:true,
+      templateUrl: 'templates/tabsController.html'
+    })
+
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
