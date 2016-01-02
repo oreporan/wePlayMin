4ec408b61530028edf5f003080382c061716246b
yahavw <yahavwin@gmail.com>
2016-01-02 13:02:29 +0200

    fix brackets

--- client/wePlayIonic/www/js/services/leagueService.js
+++ client/wePlayIonic/www/js/services/leagueService.js
@@ -1,8 +1,9 @@
 angular.module('app.services')
   .service('leagueService', function(logger, localStorageService, userService, wpRequest, paths, constants) {
 
-      var wpLogger = logger.logger("leagueService");
+    var wpLogger = logger.logger("leagueService");
 
+    return {
       var addLeague = function(name, admin, frequency, numOfPlayersPerTeam, makeTeamsAtNum, callback) {
         wpRequest.sendPost(paths.BASE_LEAGUES + paths.PATH_LEAGUES_ADDLEAGUE, {
           name: name,
@@ -109,40 +110,40 @@ angular.module('app.services')
           }
         })
       };
+    }
+  });
 
-
-
-      // .service('leagueService',['$http', '$state', function($http) {
-      //
-      //     return {
-      //         getLeagueById: function(clientId, leagueId, success, fail) {
-      //             var req = {
-      //                 method: 'GET',
-      //                 url: 'http://localhost:3000/wePlay/v1/leagues/getLeague/' + leagueId,
-      //                 headers: {
-      //                     'Content-Type': 'application/json',
-      //                     'client-id': clientId
-      //                 }
-      //             }
-      //             return $http(req).then(success, fail);
-      //         },
-      //         createLeague: function(clientId, params, success, fail){
-      //             var req = {
-      //                 method: 'POST',
-      //                 url: 'http://localhost:3000/wePlay/v1/leagues/addLeague/',
-      //                 headers: {
-      //                     'Content-Type': 'application/json',
-      //                     'client-id': clientId
-      //                 },
-      //                 data: {
-      //                     name: params.name,
-      //                     numOfPlayersPerTeam: params.numOfPlayersPerTeam,
-      //                     admin: clientId,
-      //                     frequency: params.frequency,
-      //                     makeTeamsAtNum: params.makeTeamsAtNum
-      //                 }
-      //             };
-      //             return $http(req).then(success, fail);
-      //         }
-      //     }
-      // }]);
+// .service('leagueService',['$http', '$state', function($http) {
+//
+//     return {
+//         getLeagueById: function(clientId, leagueId, success, fail) {
+//             var req = {
+//                 method: 'GET',
+//                 url: 'http://localhost:3000/wePlay/v1/leagues/getLeague/' + leagueId,
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'client-id': clientId
+//                 }
+//             }
+//             return $http(req).then(success, fail);
+//         },
+//         createLeague: function(clientId, params, success, fail){
+//             var req = {
+//                 method: 'POST',
+//                 url: 'http://localhost:3000/wePlay/v1/leagues/addLeague/',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'client-id': clientId
+//                 },
+//                 data: {
+//                     name: params.name,
+//                     numOfPlayersPerTeam: params.numOfPlayersPerTeam,
+//                     admin: clientId,
+//                     frequency: params.frequency,
+//                     makeTeamsAtNum: params.makeTeamsAtNum
+//                 }
+//             };
+//             return $http(req).then(success, fail);
+//         }
+//     }
+// }]);
